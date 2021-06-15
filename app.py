8324e9a65ab5a483570ddc40b3ca2f8cec585061
krupal vora <krupal.vora@sakec.ac.in>
2021-06-16 00:41:04 +0530

    first entry porblem

--- app.py
+++ app.py
@@ -14,58 +14,74 @@ app.config['MYSQL_DATABASE_DB'] = 'be'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 print('------------------------------------------------------------')
-""" conn = mysql.connect()
-cursor = conn.cursor()
-data = cursor.execute('SELECT * FROM urls ')
-data = cursor.fetchall()
-print(data)
-conn.commit()
-conn.close() """
-
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
     conn = mysql.connect()
     cursor = conn.cursor()
     if request.method == 'POST':
-        url = request.form['url']
-        print(url)
-        if not url:
-            flash('The URL is required!')
-            return redirect(url_for('index'))
-
-        url_id = cursor.execute('select id from urls ORDER BY id desc')
-        url_id =cursor.fetchall()
-        url_id=url_id[0]
-        url_id=url_id[0]
-        print('?????????????????????',url_id)
-        hashid = hashids.encode(url_id)
-        short_url = request.host_url + hashid
-        url_data = cursor.execute(
-            'INSERT INTO urls (original_url,new_url,extend) VALUES (%s,%s,%s)', (url, short_url, hashid))
-        conn.commit()
-        conn.close()
-        return render_template('index.html', short_url=short_url)
+        data=dict(request.form)
+        url =data['url'] 
+        custom=data['custom']
+        data = cursor.execute('SELECT extend FROM urls WHERE extend = (%s)', (custom,))
+        data = cursor.fetchone()
+        print('..............................................',data)
+        if data is None:
+            print('-----------------------------------')
+            url_id=1
+            if custom=='':
+                hashid = hashids.encode(url_id)
+                short_url = request.host_url + hashid
+            else:
+                short_url = request.host_url + custom
+                hashid=custom
+            print('****************')
+            print(url_id,url, short_url, hashid)
+            url_data = cursor.execute('INSERT INTO urls (id,original_url,new_url,extend) VALUES (%s,%s,%s,%s)', (url_id,url, short_url, hashid))
+            conn.commit()
+            conn.close()
+            return render_template('index.html', short_url=short_url)
+                  
+        else:
+            print('++++++++++++++++++++++++++++++++++++ ')
+            url_id = cursor.execute('select id from urls ORDER BY id desc')
+            url_id =cursor.fetchall()   
+            url_id=url_id[0]
+            url_id=url_id[0]
+            if data[0]!='':
+                flash('set other custom url !')
+                return redirect(url_for('index'))
+            if custom=='':
+                hashid = hashids.encode(url_id)
+                short_url = request.host_url + hashid
+            else:
+                short_url = request.host_url + custom
+                hashid=custom
+            url_data = cursor.execute('INSERT INTO urls (original_url,new_url,extend) VALUES (%s,%s,%s)', (url, short_url, hashid))
+            conn.commit()
+            conn.close()
+            return render_template('index.html', short_url=short_url)
 
     return render_template('index.html')
 
 
 @app.route('/<id>')
 def url_redirect(id):
-    print(id)
     conn = mysql.connect()
     cursor = conn.cursor()
-    original_id = hashids.decode(id)
-    print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', original_id)
+    original_id=id
     if original_id:
-        original_id = original_id[0]+1
-        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', original_id)
-        data = cursor.execute(
-            'SELECT * FROM urls WHERE id = (%s)', (original_id))
+        data = cursor.execute('SELECT * FROM urls WHERE extend = (%s)', (original_id,))
         data = cursor.fetchone()
+        id=data[0]
         original_url = data[2]
         click = data[3]
-        cursor.execute('UPDATE urls SET click = (%s) WHERE id = (%s)',(click+1, original_id))
+        new_url=data[4]
+        extend=data[5]
+        cursor.execute('UPDATE urls SET click = (%s) WHERE extend = (%s)',(click+1, original_id))
+        cursor.execute(
+            'INSERT INTO views (id,original_url,new_url,extend) VALUES (%s,%s,%s,%s)', (id,original_url,new_url, extend))
+        
         conn.commit()
         conn.close()
         return redirect(original_url)
@@ -80,16 +96,20 @@ def stats():
     db_urls = cursor.execute('SELECT * FROM urls')
     db_urls=cursor.fetchall()
     conn.close()
-    print('+++++++++++++++++++++++++++++++++++++++++++++',db_urls)
     urls = list(db_urls)
-    """ for url in db_urls:
-        url = dict(url)
-        url['short_url'] = request.host_url + hashids.encode(url['id'])
-        urls.append(url) """
-    print('+++++++++++++++++++++++++++++++++++++++++++++',urls)
     return render_template('stats.html', urls=urls)
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/delete/<string:id>',methods = ['POST', 'GET'])
+def delete(id):
+    id=int(id)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('delete from urls  where id= (%s)', (id,))    
+    conn.commit()
+    conn.close()
+    return redirect(url_for('stats'))
 if __name__ == '__main__':
     app.run(debug=True)
